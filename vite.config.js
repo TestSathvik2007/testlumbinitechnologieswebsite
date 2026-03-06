ec1d4562788e416f1fdae002015ac759b93396f2
TestSathvik2007 <nikhilkrishnasathvikedu@gmail.com>
2026-03-06 21:08:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // GitHub Pages serves this repo under a subpath in production
-  base: mode === "production" ? "/lumbini-technologies-website/" : "/",
+  base:"/",
   plugins: [react()],
   publicDir: "public",
   server: {
